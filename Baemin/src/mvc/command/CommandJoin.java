bdd9cd81e044a5a7e9d61041944d209e12bb45cf
kosta <kosta@192.168.0.102>
2017-10-18 09:22:33 +0900

    s

---
+++ Baemin/src/mvc/command/CommandJoin.java
@@ -0,0 +1,25 @@
+package mvc.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Member;
+
+public class CommandJoin implements Command {
+	private String next;
+	
+	public CommandJoin( String next ) {
+		this.next = next;
+	}
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws CommandException {
+		
+		Member m = new Member();
+		
+		
+		return next;
+	}
+	
+	
+}
